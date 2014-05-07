4ef13ce6b4dcbf588684f01b533dddbe7767cdb9
Glavin Wiechert <glavin.wiechert@gmail.com>
2014-05-07 20:57:48 -0300

    See #2. Improve Ouya Car controllers for acceleration and reverse, using button triggers.

--- Game/Assets/Sample Assets/Vehicles/Car/Scripts/CarUserControl.cs
+++ Game/Assets/Sample Assets/Vehicles/Car/Scripts/CarUserControl.cs
@@ -7,64 +7,80 @@ OuyaSDK.IMenuAppearingListener,
 OuyaSDK.IPauseListener,
 OuyaSDK.IResumeListener
 {
-	private CarController car;  // the car controller we want to use
-	public float joystickDeadzone = 0.25f;
-	public OuyaSDK.OuyaPlayer controllerIndex = OuyaSDK.OuyaPlayer.player1;
+		private CarController car;  // the car controller we want to use
+		public float joystickDeadzone = 0.25f;
+		public OuyaSDK.OuyaPlayer controllerIndex = OuyaSDK.OuyaPlayer.player1;
 	
-	void Awake()
-	{
-		// get the car controller
-		car = GetComponent<CarController>();
+		void Awake ()
+		{
+				// get the car controller
+				car = GetComponent<CarController> ();
 		
-		OuyaSDK.registerMenuButtonUpListener(this);
-		OuyaSDK.registerMenuAppearingListener(this);
-		OuyaSDK.registerPauseListener(this);
-		OuyaSDK.registerResumeListener(this);
-		Input.ResetInputAxes();
-	}
-	void OnDestroy()
-	{
-		OuyaSDK.unregisterMenuButtonUpListener(this);
-		OuyaSDK.unregisterMenuAppearingListener(this);
-		OuyaSDK.unregisterPauseListener(this);
-		OuyaSDK.unregisterResumeListener(this);
-		Input.ResetInputAxes();
-	}
+				OuyaSDK.registerMenuButtonUpListener (this);
+				OuyaSDK.registerMenuAppearingListener (this);
+				OuyaSDK.registerPauseListener (this);
+				OuyaSDK.registerResumeListener (this);
+				Input.ResetInputAxes ();
+		}
+
+		void OnDestroy ()
+		{
+				OuyaSDK.unregisterMenuButtonUpListener (this);
+				OuyaSDK.unregisterMenuAppearingListener (this);
+				OuyaSDK.unregisterPauseListener (this);
+				OuyaSDK.unregisterResumeListener (this);
+				Input.ResetInputAxes ();
+		}
 	
 	#region Ouya Listeners
-	public void OuyaMenuButtonUp()
-	{
-	}
+		public void OuyaMenuButtonUp ()
+		{
+		}
 	
-	public void OuyaMenuAppearing()
-	{
-	}
+		public void OuyaMenuAppearing ()
+		{
+		}
 	
-	public void OuyaOnPause()
-	{
-	}
+		public void OuyaOnPause ()
+		{
+		}
 	
-	public void OuyaOnResume()
-	{
-	}
+		public void OuyaOnResume ()
+		{
+		}
 	#endregion
 	
-	void FixedUpdate()
-	{
-		// pass the input to the car!
-		
-		Vector2 leftStick = new Vector2(0f,0f);
-		
-		if (Mathf.Abs(OuyaExampleCommon.GetAxis(OuyaSDK.KeyEnum.AXIS_LSTICK_X, controllerIndex)) > joystickDeadzone)
+		void FixedUpdate ()
 		{
-			leftStick.x = OuyaExampleCommon.GetAxis(OuyaSDK.KeyEnum.AXIS_LSTICK_X, controllerIndex);
-		}
+				// pass the input to the car!
+
+				Vector2 leftStick = new Vector2 (0f, 0f);
+
+				// PC Controller
+				leftStick.x = Input.GetAxis ("Horizontal");
+				leftStick.y = -Input.GetAxis ("Vertical");
+
+				// Ouya Controller
+				// Left/Right
+				if (Mathf.Abs (OuyaExampleCommon.GetAxis (OuyaSDK.KeyEnum.AXIS_LSTICK_X, controllerIndex)) > joystickDeadzone) {
+						leftStick.x = OuyaExampleCommon.GetAxis (OuyaSDK.KeyEnum.AXIS_LSTICK_X, controllerIndex);
+				}
+				// Forward/Reverse
+				/*
+				if (Mathf.Abs(OuyaExampleCommon.GetAxis(OuyaSDK.KeyEnum.AXIS_LSTICK_Y, controllerIndex)) > joystickDeadzone)
+				{
+					leftStick.y = OuyaExampleCommon.GetAxis(OuyaSDK.KeyEnum.AXIS_LSTICK_Y, controllerIndex);
+				}
+				*/
+
+				float forward = OuyaExampleCommon.GetAxis (OuyaSDK.KeyEnum.BUTTON_RT, controllerIndex);
+				float reverse = OuyaExampleCommon.GetAxis (OuyaSDK.KeyEnum.BUTTON_LT, controllerIndex);
+				
+				float y = -(forward - reverse);
+				if (Mathf.Abs(y) > joystickDeadzone) {
+					leftStick.y = y;
+				}
 		
-		if (Mathf.Abs(OuyaExampleCommon.GetAxis(OuyaSDK.KeyEnum.AXIS_LSTICK_Y, controllerIndex)) > joystickDeadzone)
-		{
-			leftStick.y = OuyaExampleCommon.GetAxis(OuyaSDK.KeyEnum.AXIS_LSTICK_Y, controllerIndex);
+				car.Move (leftStick.x, -leftStick.y); //-y because Unity 4.3 inverts it
 		}
-		
-		car.Move(leftStick.x, -leftStick.y); //-y because Unity 4.3 inverts it
-	}
 }
